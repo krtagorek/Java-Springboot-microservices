4003b4ed9b70541ba62c68911bb37d7fdb31ecbb
tagore <krtagorek@gmail.com>
2024-10-20 18:41:26 -0500

    added different spring cloud config server profiles

---
+++ currency-exchange-service/src/test/java/com/rxk163/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxk163.microservices.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
