bdc8a0cc88f837ea2f265ba99a466a145a0d8f7e
tagore <krtagorek@gmail.com>
2024-10-27 17:03:55 -0500

    Implemented API Gateway.

---
+++ api-gateway/src/test/java/com/rxk163/microservices/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxk163.microservices.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
